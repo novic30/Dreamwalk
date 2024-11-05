3fd1a13814126336617a8b9b56e264fc81a030e0
HoppyBall <21787@students.isb.ac.th>
2024-11-05 13:21:39 +0700

    camera

--- Dreamwalk/Assets/camera_folow.cs
+++ Dreamwalk/Assets/camera_folow.cs
@@ -6,9 +6,17 @@ public class camera_folow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public int min;
+    private Vector3 nulls;
+
 
     void LateUpdate ()
     {
+        nulls = target.position
+        if ( target.position > new Vector3(nulls.x,15,nulls.z))
+        {
+            target.y = 15;
+        }
         transform.position = target.position + offset;
     }
 }
