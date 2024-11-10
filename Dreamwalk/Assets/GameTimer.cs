8a127f491af395df329d37e286683d8b62216eb2
BadTM <128948869+BadTM@users.noreply.github.com>
2024-11-10 22:21:56 +0700

    q

--- Dreamwalk/Assets/GameTimer.cs
+++ Dreamwalk/Assets/GameTimer.cs
@@ -1,34 +1,53 @@
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 public class GameTimer : MonoBehaviour
 {
     public Text timerText;  
     private float timer = 0f;
-    private bool isTimerRunning = true; 
-    public GameObject mc;
+    private bool isTimerRunning = false;  
+    public GameObject mc;  
+    public float startPositionX = -400f; 
+    public float stopPositionX = -210f; 
     public float stopDistance = 1f;  
+
+    
     void Start()
-    {   
+    {
+       
+        timer = 0f;
+        isTimerRunning = false;
     }
 
- 
+   
     void Update()
     {
+       
+        if (!isTimerRunning && mc.transform.position.x >= startPositionX - stopDistance)
+        {
+            StartTimer();
+        }
+
+        
         if (isTimerRunning)
         {
             timer += Time.deltaTime;
-            timerText.text = "Time: " + timer.ToString("F2"); 
-            
-            
+            timerText.text = "Time: " + timer.ToString("F2");
         }
-            
-            if (mc.transform.position.x >= -210f)
-            {
-                StopTimer();
-            }
+
+        
+        if (isTimerRunning && mc.transform.position.x >= stopPositionX - stopDistance)
+        {
+            StopTimer();
+        }
     }
+
     
+    void StartTimer()
+    {
+        isTimerRunning = true;
+        Debug.Log("Timer Started");
+    }
 
     
     void StopTimer()
